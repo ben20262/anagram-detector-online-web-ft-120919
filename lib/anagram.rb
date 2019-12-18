8a2a0f189c3bf6749501568eff920b8b7f46ca75
Benjamin Small <ben20262@aim.com>
2019-12-18 21:16:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    anagram = ""
+    if @name.length == list.length
+      list.each do |anag|
+        return anag if @word.split("").sort == anag.split("").sort
+      end
+    end
+  end
+
+end
